478d3d48401791c1d1f1a493447e82eaa078abb7
yaosiang.su <yaosiang.su@104.com.tw>
2020-07-27 16:34:48 +0800

    fix wrong coding style

--- test/mock/driving-licence/valid-applicant.ts
+++ test/mock/driving-licence/valid-applicant.ts
@@ -1,37 +1,31 @@
 import { ILicenceApplicant } from '../../../src/mock/driving-licence/licence-applicant';
 
-export class ValidApplicant implements ILicenceApplicant
-{
+export class ValidApplicant implements ILicenceApplicant {
   private readonly initials: string;
 
   public constructor(initials: string) {
     this.initials = initials;
   }
 
-  public getAge(): number
-  {
+  public getAge(): number {
     return 21;
   }
 
-  public holdsLicence(): boolean
-  {
+  public holdsLicence(): boolean {
     return false;
   }
 
-  public getId(): number
-  {
+  public getId(): number {
     return 123;
   }
 
-  public getDateOfBirth(): Date
-  {
+  public getDateOfBirth(): Date {
     // Date 的月份是從 0 開始，11 月需要傳入 10
     // ref: https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Date
     return new Date(1999, 10, 7);
   }
 
-  public getInitials(): string
-  {
+  public getInitials(): string {
     return this.initials;
   }
 }
