860eb8aed35662e0ffeec8efa6851f07acf7bdb4
yaosiang.su <yaosiang.su@104.com.tw>
2020-07-30 11:00:23 +0800

    add comments

--- src/mock/driving-licence/driving-licence-generator.ts
+++ src/mock/driving-licence/driving-licence-generator.ts
@@ -14,21 +14,27 @@ export class DrivingLicenceGenerator {
   }
 
   public generateNumber(applicant: ILicenceApplicant): string {
+    // 只有 18 歲以上成年人才可以領取駕照
     if (applicant.getAge() < 17) {
       this.logger.warn(`Under age application user: ${applicant.getId()}`);
       throw new InvalidDriverException('Applicant is too young');
     }
 
+    // 一個人只能領取一次駕照
     if (applicant.holdsLicence()) {
       this.logger.warn(`duplicate application user: ${applicant.getId()}`);
       throw new InvalidDriverException('Cannot hold more than one licence');
     }
 
+    // 名字的起首字母+MMDDYYYY => a
     const licence = applicant.getInitials() + dayjs(applicant.getDateOfBirth()).format('MMDDYYYY');
 
+    // 15 減去 a 的長度為 b 的長度
     let numberOfDigits: number = 15 - licence.length;
+    // 但是 b 的長度最少會有 4 個字
     numberOfDigits = (numberOfDigits < 4) ? 4 : numberOfDigits;
 
+    // 格式為 a + b
     return `${licence}${this.randomNumbersGenerator.generate(numberOfDigits)}`;
   }
 }
