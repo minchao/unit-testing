3fac7b9137c552aa3ee6473d3785a387ba9e07ff
yaosiang.su <yaosiang.su@104.com.tw>
2020-07-30 15:18:49 +0800

    add unused user to test case

--- test/di/03-remove-trip-dao/trip-service.test.ts
+++ test/di/03-remove-trip-dao/trip-service.test.ts
@@ -9,13 +9,14 @@ import { MockTripDAO } from './MockTripDAO';
 describe('Trip Service', () => {
 
   const guest: User | null = null;
+  const unusedUser: User | null = null;
   const registeredUser: User = new User();
   const otherUser: User = new User();
 
   it('should_Throw_Exception_When_User_Is_Not_LoggedIn', () => {
     const tripService: TripService = Container.get(TripService);
 
-    expect(() => { tripService.getTripsByUser(null, guest); }).toThrowError(UserNotLoggedInException);
+    expect(() => { tripService.getTripsByUser(unusedUser, guest); }).toThrowError(UserNotLoggedInException);
   });
 
   it('should_Not_Return_Trips_When_Logged_User_Are_Not_Friend', () => {
