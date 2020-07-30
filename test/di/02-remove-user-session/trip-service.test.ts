832adb97af8af2f20335f979ab50a5bd15521ff4
yaosiang.su <yaosiang.su@104.com.tw>
2020-07-30 07:13:23 +0000

    add unused user to test case

--- test/di/02-remove-user-session/trip-service.test.ts
+++ test/di/02-remove-user-session/trip-service.test.ts
@@ -6,13 +6,14 @@ import { User } from '../../../src/di/user/refactoring/02-remove-user-session/Us
 describe('Trip Service', () => {
 
   const guest: User | null = null;
+  const unusedUser: User | null = null;
   const registeredUser: User = new User();
   const otherUser: User = new User();
 
   it('should_Throw_Exception_When_User_Is_Not_LoggedIn', () => {
     const tripService: TripService = new TestableTripService();
 
-    expect(() => { tripService.getTripsByUser(null, guest); }).toThrowError(UserNotLoggedInException);
+    expect(() => { tripService.getTripsByUser(unusedUser, guest); }).toThrowError(UserNotLoggedInException);
   });
 
   it('should_Not_Return_Trips_When_Logged_User_Are_Not_Friend', () => {
