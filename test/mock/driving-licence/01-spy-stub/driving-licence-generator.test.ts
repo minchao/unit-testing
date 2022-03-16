fca9fc7cb1867b8a9aa28a16adb7147281bdd46d
Yao-Siang Su <yaosiang.su@104.com.tw>
2022-03-16 15:33:21 +0800

    update driving-licence-generator test case

--- test/mock/driving-licence/01-spy-stub/driving-licence-generator.test.ts
+++ test/mock/driving-licence/01-spy-stub/driving-licence-generator.test.ts
@@ -7,7 +7,7 @@ import { SpyLogger } from './spy-logger';
 import { UnderAgeApplicant } from './under-age-applicant';
 import { LicenceHolderApplicant } from './licence-holder-applicant';
 
-xdescribe('Driving Licence', () => {
+describe('Driving Licence', () => {
   let logger: SpyLogger;
   let random: MockRandomNumbersGenerator;
   let generator: DrivingLicenceGenerator;
@@ -18,7 +18,14 @@ xdescribe('Driving Licence', () => {
     generator = new DrivingLicenceGenerator(logger, random);
   });
 
-  it('testUnderAgeApplicationsAreLogged', () => {
+  // 為了補齊以下測試案例，你需要準備對應的 Stub
+
+  xit('testUnderAgeApplicantCannotGenerateLicence', () => {
+    // 嘗試看看 assert 會拋出 Exception 的狀況
+    // 可以參考 https://jestjs.io/docs/en/expect.html#tothrowerror
+  });
+
+  xit('testUnderAgeApplicationsAreLogged', () => {
     const applicant: ILicenceApplicant = new UnderAgeApplicant();
 
     try {
@@ -30,18 +37,30 @@ xdescribe('Driving Licence', () => {
     expect('Under age application user: 123').toEqual(logger.warnLastMessage);
   });
 
-  xit('testUnderAgeApplicantCannotGenerateLicence', () => {
-    // 嘗試看看 assert 會拋出 Exception 的狀況
-    // 可以參考 https://jestjs.io/docs/en/expect.html#tothrowerror
+  xit('testLicenceHolderCannotGenerateLicence', () => {
+    // 請補齊測試案例
   });
-
-  // 為了補齊以下測試案例，你需要準備對應的 Stub
 
   xit('testLicenceHolderAttemptsLogged ', () => {
     // 請補齊測試案例
   });
 
-  xit('testLicenceHolderCannotGenerateLicence', () => {
-    // 請補齊測試案例
+  xit('駕照號碼至少需要 15 個字元', () => {
+    // const applicant1: ILicenceApplicant = new Stub();
+    // const applicant2: ILicenceApplicant = new Stub();
+    // const applicant3: ILicenceApplicant = new Stub();
+    const randomMap: Map<number, string> = new Map<number, string>();
+
+    // Map(隨機碼長度, 隨機碼數字)
+    random.mockGenerate(
+      randomMap
+        .set(4, '0123')
+        .set(5, '01234')
+        .set(6, '012345')
+    );
+
+    // expect('').toEqual(generator.generateNumber(applicant1));
+    // expect('').toEqual(generator.generateNumber(applicant2));
+    // expect('').toEqual(generator.generateNumber(applicant3));
   });
 });
--- test/mock/driving-licence/01-spy-stub/mock-random-numbers-generator.ts
+++ test/mock/driving-licence/01-spy-stub/mock-random-numbers-generator.ts
@@ -1,7 +1,14 @@
 import { IRandomNumberGenerator } from '../../../../src/mock/driving-licence/random-number-generator';
 
 export class MockRandomNumbersGenerator implements IRandomNumberGenerator {
-  public generate(numberOfDigits: number): string | undefined {
+
+  private responses: Map<number, string>;
+
+  public mockGenerate(callAndResponse: Map<number, string>): void {
+    this.responses = callAndResponse;
+  }
+
+  public generate(numberOfDigits: number): string {
     return '';
   }
 }
